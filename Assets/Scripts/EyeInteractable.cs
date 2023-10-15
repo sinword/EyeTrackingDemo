e412d12e0f0ad9672baa9ef194e2596101319c35
sinword <samwang9199@gmail.com>
2023-10-15 23:54:45 +0800

    EyeTracking completed

---
+++ Assets/Scripts/EyeInteractable.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+public class EyeInteractable : MonoBehaviour
+{
+    public bool IsHovered { get; set; }
+
+    [SerializeField]
+    private UnityEvent<GameObject> OnObjectHover;
+
+    [SerializeField]
+    private Material OnHoverActiveMaterial;
+
+    [SerializeField]
+    private Material OnHoverInactiveMaterial;
+
+    private MeshRenderer meshRenderer;
+
+    void Start() => meshRenderer = GetComponent<MeshRenderer>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsHovered)
+        {
+            meshRenderer.material = OnHoverActiveMaterial;
+            OnObjectHover?.Invoke(gameObject);
+        }
+        else
+        {
+            meshRenderer.material = OnHoverInactiveMaterial;
+        }
+    }
+}
